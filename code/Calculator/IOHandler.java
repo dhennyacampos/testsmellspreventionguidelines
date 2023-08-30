66983c19d004613a9e65608fa18c44ba0aee39b2
Luana <32377084+luana-martins@users.noreply.github.com>
2023-08-30 08:16:33 +0200

    Calculator files

---
+++ code/Calculator/IOHandler.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class IOHandler {
+    public List<String> readOperationsFromFile(String fileName) throws IOException {
+        List<String> operations = new ArrayList<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                operations.add(line);
+            }
+        }
+        return operations;
+    }
+
+    public void writeResultsToFile(List<String> results, String fileName) throws IOException {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName))) {
+            for (String result : results) {
+                writer.write(result);
+                writer.newLine();
+            }
+        }
+    }
+}
---
+++ code/Calculator/MathOperations.java
@@ -0,0 +1,30 @@
+public class MathOperations {
+    public double evaluateOperation(String operation) throws Exception {
+
+        // For simplicity, assume the input format is: "operand operator operand"
+        String[] tokens = operation.split(" ");
+        if (tokens.length != 3) {
+            throw new IllegalArgumentException("Invalid operation format");
+        }
+
+        double operand1 = Double.parseDouble(tokens[0]);
+        String operator = tokens[1];
+        double operand2 = Double.parseDouble(tokens[2]);
+
+        switch (operator) {
+            case "+":
+                return operand1 + operand2;
+            case "-":
+                return operand1 - operand2;
+            case "*":
+                return operand1 * operand2;
+            case "/":
+                if (operand2 == 0) {
+                    throw new ArithmeticException("Division by zero");
+                }
+                return operand1 / operand2;
+            default:
+                throw new IllegalArgumentException("Invalid operator");
+        }
+    }
+}
